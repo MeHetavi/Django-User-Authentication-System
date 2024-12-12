da4f6e0b04cfaedab0254a95b9e7159155a9648a
MeHetavi <hetavi.shah05@gmail.com>
2024-12-12 10:47:39 +0530

    Updated.

--- app/user/admin.py
+++ app/user/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
+from .models import CustomUser
 
 # Register your models here.
+
+class CustomUserAdmin(UserAdmin):
+    model = CustomUser
+    fieldsets = UserAdmin.fieldsets + (
+        (None, {'fields': ('last_updated',)}),
+    )
+    readonly_fields = ('last_updated',)
+
+admin.site.register(CustomUser, CustomUserAdmin)
